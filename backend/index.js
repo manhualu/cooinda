9cd0cb82e09420f5194917eac572b2bbcc40b4d6
Oliver Xu <oliver.xu1@student.unsw.edu.au>
2023-07-01 15:43:02 +1000

    deez nutz

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/deeznutz", (req, res) => {
+  res.json({ yourMum: "nuts" });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
