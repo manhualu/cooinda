cf78f047adae0acb3476c01f171fb4e5aa0b41ef
Brendan <z5209522@ad.unsw.edu.au>
2023-07-01 16:49:01 +1000

    im him

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.use(express.json());
+
+const emergencyCountries = JSON.parse(fs.readFileSync('./emergency.json', 'utf8'));
+function getEmergency(country) {
+    for (const eachCountry of emergencyCountries) {
+        if (eachCountry.Country.Name.toLowerCase() === country.toLowerCase()) {
+            return(`Police number for ${country}: ${eachCountry.Police.All}`);
+        }
+    }
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -12,6 +22,20 @@ app.get("/deeznutz", (req, res) => {
   res.json({ yourMum: "nuts" });
 });
 
+app.get("/when", (req, res) => {
+    res.json({ did: "iAsk" });
+  });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.get("/help/:country", (req, res) => {
+    const country = req.params.country;
+    console.log(country);
+    res.json(getEmergency(country));
+  });
+
+
+console.log(getEmergency("Australia"));
+// getEmergency("australia");
